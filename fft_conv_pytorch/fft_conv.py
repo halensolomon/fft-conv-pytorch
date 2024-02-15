d789a137ff464fb3fe1c3cfff02eac224198fc4f
Halen Solomon <101902768+halensolomon@users.noreply.github.com>
2024-02-14 22:21:46 -0500

    Update fft_conv.py
    Removed complex_matmul, since it is now natively supported in PyTorch

--- fft_conv_pytorch/fft_conv.py
+++ fft_conv_pytorch/fft_conv.py
@@ -8,27 +8,27 @@ from torch.fft import irfftn, rfftn
 from math import ceil, floor
 
 
-def complex_matmul(a: Tensor, b: Tensor, groups: int = 1) -> Tensor:
-    """Multiplies two complex-valued tensors."""
-    # Scalar matrix multiplication of two tensors, over only the first channel
-    # dimensions. Dimensions 3 and higher will have the same shape after multiplication.
-    # We also allow for "grouped" multiplications, where multiple sections of channels
-    # are multiplied independently of one another (required for group convolutions).
-    a = a.view(a.size(0), groups, -1, *a.shape[2:])
-    b = b.view(groups, -1, *b.shape[1:])
+# def complex_matmul(a: Tensor, b: Tensor, groups: int = 1) -> Tensor:
+#     """Multiplies two complex-valued tensors."""
+#     # Scalar matrix multiplication of two tensors, over only the first channel
+#     # dimensions. Dimensions 3 and higher will have the same shape after multiplication.
+#     # We also allow for "grouped" multiplications, where multiple sections of channels
+#     # are multiplied independently of one another (required for group convolutions).
+#     a = a.view(a.size(0), groups, -1, *a.shape[2:])
+#     b = b.view(groups, -1, *b.shape[1:])
 
-    a = torch.movedim(a, 2, a.dim() - 1).unsqueeze(-2)
-    b = torch.movedim(b, (1, 2), (b.dim() - 1, b.dim() - 2))
+#     a = torch.movedim(a, 2, a.dim() - 1).unsqueeze(-2)
+#     b = torch.movedim(b, (1, 2), (b.dim() - 1, b.dim() - 2))
 
-    # complex value matrix multiplication
-    real = a.real @ b.real - a.imag @ b.imag
-    imag = a.imag @ b.real + a.real @ b.imag
-    real = torch.movedim(real, real.dim() - 1, 2).squeeze(-1)
-    imag = torch.movedim(imag, imag.dim() - 1, 2).squeeze(-1)
-    c = torch.zeros(real.shape, dtype=torch.complex64, device=a.device)
-    c.real, c.imag = real, imag
+#     # complex value matrix multiplication
+#     real = a.real @ b.real - a.imag @ b.imag
+#     imag = a.imag @ b.real + a.real @ b.imag
+#     real = torch.movedim(real, real.dim() - 1, 2).squeeze(-1)
+#     imag = torch.movedim(imag, imag.dim() - 1, 2).squeeze(-1)
+#     c = torch.zeros(real.shape, dtype=torch.complex64, device=a.device)
+#     c.real, c.imag = real, imag
 
-    return c.view(c.size(0), -1, *c.shape[3:])
+#     return c.view(c.size(0), -1, *c.shape[3:])
 
 
 def to_ntuple(val: Union[int, Iterable[int]], n: int) -> Tuple[int, ...]:
@@ -128,7 +128,7 @@ def fft_conv(
     kernel_fr = rfftn(padded_kernel.float(), dim=tuple(range(2, signal.ndim)))
 
     kernel_fr.imag *= -1
-    output_fr = complex_matmul(signal_fr, kernel_fr, groups=groups)
+    output_fr = torch.mul(signal_fr, kernel_fr)
     output = irfftn(output_fr, dim=tuple(range(2, signal.ndim)))
 
     # Remove extra padded values
